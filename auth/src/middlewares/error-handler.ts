5f862a6233c96f5077410de0a718c3ca6a206936
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-07 16:20:27 -0400

    Format errors to provide same structure

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -10,14 +10,22 @@ export const errorHandler = (
 ) => {
     
     if(err instanceof RequestValidationError) {
-        console.log('handling this error as a request validation error')
+        const formattedErrors = err.errors.map(error => {
+            return {
+                message: error.msg,
+                field: (error.type === 'field') && error.path || ""
+            }
+        })
+        return res.status(400).send({ errors: formattedErrors })
     }
 
     if(err instanceof DatabaseConnectionError) {
-        console.log('handling this error as a database connection error')
+        return res.status(500).send({ errors: [
+            { message: err.reason }
+        ] })
     }
 
     res.status(400).send({
-        message: err.message
+        errors: [{ message: 'Something went wrong'}]
     })
 }
