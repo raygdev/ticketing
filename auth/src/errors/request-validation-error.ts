d7c465a91671429d36097ebc8ee264f056e10237
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-07 16:34:31 -0400

    Add serializeErrors function to both classes

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,10 +1,21 @@
 import { ValidationError } from "express-validator";
 
 export class RequestValidationError extends Error {
+    statusCode = 400
     constructor(public  errors: ValidationError[]) {
         super()
 
         // Only because we are extending a build in class
         Object.setPrototypeOf(this, RequestValidationError.prototype)
+    }
+
+    serializeErrors() {
+        const formattedErrors = this.errors.map(error => {
+            return {
+                message: error.msg,
+                field: (error.type === 'field') && error.path || ""
+            }
+        })
+        return formattedErrors
     }
 }
