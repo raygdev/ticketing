4ab79d50f4affe7a7c10faf40fe9edca9d47c4e6
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-07 21:02:01 -0400

    Create presave hook for password hashing

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose, { mongo } from "mongoose";
+import { Password } from "../services/password";
 
 // an interface that describes the properties
 // that are required to create a new user.
@@ -24,6 +25,14 @@ interface UserDoc extends mongoose.Document {
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: true}
+})
+
+userSchema.pre('save', async function(done) {
+    if(this.isModified('password')) {
+        const hashed = await Password.toHash(this.get('password'))
+        this.set('password', hashed)
+    }
+    done()
 })
 
 userSchema.statics.build = (attrs: UserAttrs) => {
