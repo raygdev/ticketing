5f18d579c369a5110c7acdb887131ab898fadbe0
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-07 20:12:32 -0400

    Create model for user

---
+++ auth/src/models/user.ts
@@ -0,0 +1,35 @@
+import mongoose, { mongo } from "mongoose";
+
+// an interface that describes the properties
+// that are required to create a new user.
+
+interface UserAttrs {
+    email: string,
+    password: string
+}
+
+// an interface that describes the properties
+// that the user model has
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc
+}
+
+// an interface that describes the properties
+// that the user document has
+interface UserDoc extends mongoose.Document {
+    email: string;
+    password: string;
+}
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true}
+})
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+    return new User(attrs)
+}
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema)
+
+export { User }
