2e39cd63e6c0b1f8b3374fd80be9aab8d91cd2a8
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-08 08:36:24 -0400

    Add validation and types for req and res

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,18 @@
-import express from 'express'
+import express, { Request, Response} from 'express'
+import { body } from 'express-validator'
 const router = express.Router()
 
-router.post("/api/users/signin", (req, res) => {
+router.post("/api/users/signin",
+[
+  body('email')
+    .isEmail()
+    .withMessage('Email must be valid'),
+  body('password')
+    .trim()
+    .notEmpty()
+    .withMessage('You must supply a password')
+],
+(req: Request, res: Response) => {
   res.send("Hi there")
 })
 
