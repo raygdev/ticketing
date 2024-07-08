0963a9cb03feaf40a0528af5c178f164e0c96f56
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-08 08:31:45 -0400

    Remove version key and password when sending user

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -25,6 +25,15 @@ interface UserDoc extends mongoose.Document {
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: true}
+}, {
+    toJSON: {
+        transform(doc, ret) {
+            ret.id = ret._id
+            delete ret._id
+            delete ret.password
+        },
+        versionKey: false
+    }
 })
 
 userSchema.pre('save', async function(done) {
