cb73746fe9bfe44ae5c11c3468ec893b73316079
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-08 15:06:42 -0400

    Clean up test and user global for cookie

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,18 +1,8 @@
 import request from 'supertest'
 import { app } from '../../app'
 
-const userCreds = {
-    email: 'test@test.com',
-    password: 'password'
-}
-
 it('responsed with details about the current user', async () => {
-    const authResponse = await request(app)
-      .post('/api/users/signup')
-      .send(userCreds)
-      .expect(201)
-
-    const cookie = authResponse.get('Set-Cookie')
+    const cookie = await signin()
 
     const response = await request(app)
       .get('/api/users/currentuser')
