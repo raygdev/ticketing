e1ff7acd1543a2a973a667ae6deb0608ffd8452c
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-08 22:18:52 -0400

    Import axios and make request for signup

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,11 +1,23 @@
 import { useState } from "react";
+import axios from 'axios'
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    const response = await axios.post('/api/users/signup', {
+        email,
+        password
+    })
+
+    console.log(response.data)
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign up</h1>
       <div className="form-group">
         <label>Email Address</label>
