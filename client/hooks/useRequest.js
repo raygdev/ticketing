407f8facbe5ec64bb0f235a6eb2e1e11e17e261c
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-09 21:10:28 -0400

    Create onSuccess callback

--- client/hooks/useRequest.js
+++ client/hooks/useRequest.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 import { useState } from "react";
 
-function useRequest({ url, method, body }) {
+function useRequest({ url, method, body, onSuccess }) {
   const [errors, setErrors] = useState([]);
 
   const doRequest = async () => {
     try {
       setErrors(null)
       const response = await axios[method](url, body);
+
+      if(onSuccess) {
+        onSuccess(response.data)
+      }
+
       return response.data;
     } catch (e) {
         console.log(e, 'error')
