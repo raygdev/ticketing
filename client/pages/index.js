12671f55e2333d9132884d4dfcc492c10e407167
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-12 20:49:22 -0400

    Use getInitial props and forward request
    checks for current user and forwards incoming request to ingress
    the pattern is HTTP://<SERVICENAME>.<NAMESPACE>.svc.cluster.local/<ENDPOINT>

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,30 @@
-const Landing = () => {
-    return <h1>Landing Page</h1>
+import axios from 'axios'
+
+const Landing = ({ currentUser }) => {
+    return <h1>Landing Page {currentUser?.email}</h1>
+}
+
+Landing.getInitialProps = async ({ req /**Incoming request to ingress (or server) */}) => {
+    try{
+        if(typeof window === 'undefined') {
+            //we are on the server
+            console.log('server')
+            const response = await axios.get(
+                'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+                    //forward the incoming headers
+                    headers: req.headers
+                }
+            )
+            return response.data
+        } else {
+            // we are on the browser
+            const response = await axios.get('/api/users/currentuser')
+            return response.data
+        }
+    } catch(e) {
+        console.log(e)
+        return {}
+    }
 }
 
 export default Landing
