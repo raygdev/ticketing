4ad04348d40674c0474164f1e9a56c956ce9b91f
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-14 18:33:24 -0400

    Add tests for invalid title and price

--- tickets/src/routes/__tests__/new.test.ts
+++ tickets/src/routes/__tests__/new.test.ts
@@ -23,10 +23,40 @@ it('returns a status other than 401 if the user is signed in', async () => {
       expect(response.status).not.toEqual(401)
 })
 it('returns an error if an invalid title is provided', async () => {
+    await request(app)
+      .post('/api/tickets')
+      .set('Cookie', signin())
+      .send({
+        title:'',
+        price: 10
+      })
+      .expect(400)
 
+    await request(app)
+      .post('/api/tickets')
+      .set('Cookie', signin())
+      .send({
+        price: 10
+      })
+      .expect(400)
 })
 it('returns an error if an invalid price is provided', async () => {
+    await request(app)
+      .post('/api/tickets')
+      .set('Cookie', signin())
+      .send({
+        title: 'title',
+        price: -10
+      })
+      .expect(400)
 
+    await request(app)
+      .post('/api/tickets')
+      .set('Cookie', signin())
+      .send({
+        title: 'title'
+      })
+      .expect(400)
 })
 it('creates a ticket with valid inputs', async () => {
 
