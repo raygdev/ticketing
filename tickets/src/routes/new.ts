6c685f149495df91fea8b2d2290bdb53698a6c5f
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-14 18:41:23 -0400

    Add validation for price

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -7,7 +7,15 @@ const router = express.Router();
 router.post(
   "/api/tickets",
   requireAuth,
-  [body("title").not().isEmpty().withMessage("title is required")],
+  [
+    body("title")
+      .not()
+      .isEmpty()
+      .withMessage("title is required"),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0')
+],
   validateRequest,
   (req: Request, res: Response) => {
     res.sendStatus(200);
