2fd87a9b6496cce31dde0408d49e87aff7f0a554
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-13 22:55:52 -0400

    Use mongo uri env variable

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,9 +6,12 @@ const start = async () => {
     if(!process.env.JWT_KEY) {
         throw new Error('No secret key defined')
     }
+    if(!process.env.MONGO_URI) {
+        throw new Error('No mongo URI defined')
+    }
 
     try {
-        await mongoose.connect("mongodb://tickets-mongo-srv:27017/auth")
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("Connected to mongodb")
     } catch(e) {
         console.error(e)
