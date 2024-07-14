e61ebc463c3ad5bc329b7a6178a6dbafa9767cf7
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-14 18:39:06 -0400

    Add validations for title

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,17 @@
-import express, { Request, Response } from 'express'
-import { requireAuth } from '@raygdevtickets/common'
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import { requireAuth, validateRequest } from "@raygdevtickets/common";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
-    res.sendStatus(200)
-})
+router.post(
+  "/api/tickets",
+  requireAuth,
+  [body("title").not().isEmpty().withMessage("title is required")],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  }
+);
 
-export { router as createTicketRouter}
+export { router as createTicketRouter };
