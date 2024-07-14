fc2c669b1617e95403bed686feabdadb003aa3ce
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-14 18:47:48 -0400

    Add test for creating ticket

--- tickets/src/routes/__tests__/new.test.ts
+++ tickets/src/routes/__tests__/new.test.ts
@@ -59,5 +59,14 @@ it('returns an error if an invalid price is provided', async () => {
       .expect(400)
 })
 it('creates a ticket with valid inputs', async () => {
+    // add in  a check to make sure a ticket was saved
 
+    await request(app)
+      .post('/api/tickets')
+      .set('Cookie', signin())
+      .send({
+        title: 'title',
+        price: 20
+      })
+      .expect(201)
 })
