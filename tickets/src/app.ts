52f78ea1cce8bc44178359d62d0b1320377cc835
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-14 00:51:37 -0400

    Import route and package middleware

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import 'express-async-errors'
 import cookieSession from 'cookie-session'
-import { errorHandler, NotFoundError } from '@raygdevtickets/common'
+import { errorHandler, NotFoundError, currentUser } from '@raygdevtickets/common'
+
+import { createTicketRouter } from './routes/new'
 
 const app = express()
 app.set('trust proxy', true)
@@ -10,6 +12,9 @@ app.use(cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== 'test'
 }))
+
+app.use(currentUser)
+app.use(createTicketRouter)
 
 app.all("*", async () => {
     throw new NotFoundError()
