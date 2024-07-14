31a96dc5f271c6b7aed1e4dae9a1e18cb8974766
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-14 00:50:53 -0400

    Modify global signin and mock cookie

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,27 +1,34 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
+import jwt from 'jsonwebtoken'
 import request from 'supertest'
 import { app } from '../app'
 
 declare global {
-    var signin: () => Promise<string[] | undefined>
+    var signin: () => string[]
 }
 
-global.signin = async () => {
-    const email = 'test@test.com'
-    const password = 'password'
+global.signin = () => {
+    // Build a JWT payload. { id, email }
+    const payload = {
+        id: '1lk24j124l',
+        email: 'test@test.com'
+    }
 
-    const response = await request(app)
-      .post('/api/users/signup')
-      .send({
-        email,
-        password
-      })
-      .expect(201)
+    // Create the JWT
+    const token = jwt.sign(payload, process.env.JWT_KEY!)
 
-    const cookie = response.get('Set-Cookie')
+    // Build session Object. { jwt: MY_JWT }
+    const session = { jwt: token }
 
-    return cookie
+    // Turn that session into JSON
+    const sessionJSON = JSON.stringify(session)
+
+    // Take JSON and encode it as base64
+    const base64 = Buffer.from(sessionJSON).toString('base64')
+    
+    // return a string thats the cookie with the encoded data
+    return [`session=${base64}`]
 }
 
 let mongo: any;
