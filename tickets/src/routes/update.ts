b5d59d2c8c56ff51f3eaa889fd0e361111a4319e
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-15 12:20:54 -0400

    Hook up middleware and find ticket

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -10,8 +10,16 @@ import { Ticket } from '../models/ticket'
 
 const router = express.Router()
 
-router.put('/api/tickets/:id', (req: Request, res: Response) => {
+router.put('/api/tickets/:id',
+requireAuth,
+async (req: Request, res: Response) => {
+  const ticket = await Ticket.findById(req.params.id)
 
+  if(!ticket) {
+    throw new NotFoundError()
+  }
+
+  res.send(ticket)
 })
 
 export { router as updateTicketRouter}
