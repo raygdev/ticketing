644bc79a72e0dc1b0aa5c85336909e74a295db85
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-14 20:27:45 -0400

    Add route to app

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,7 @@ import cookieSession from 'cookie-session'
 import { errorHandler, NotFoundError, currentUser } from '@raygdevtickets/common'
 
 import { createTicketRouter } from './routes/new'
+import { showTicketRouter } from './routes/show'
 
 const app = express()
 app.set('trust proxy', true)
@@ -15,6 +16,7 @@ app.use(cookieSession({
 
 app.use(currentUser)
 app.use(createTicketRouter)
+app.use(showTicketRouter)
 
 app.all("*", async () => {
     throw new NotFoundError()
