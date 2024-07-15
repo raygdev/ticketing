88ca1b50fbabb9c8576dcdee1488bb2792dadab4
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-15 13:59:02 -0400

    Test for invalid input

--- tickets/src/routes/__tests__/update.test.ts
+++ tickets/src/routes/__tests__/update.test.ts
@@ -42,7 +42,32 @@ it('returns a 401 if the user does not own the ticket', async () => {
       .expect(401)
 })
 it('returns a 400 if the user provides an invalid title and price', async () => {
+    const cookie = signin()
+    const response = await request(app)
+      .put(`/api/tickets/`)
+      .set('Cookie', cookie)
+      .send({
+        title: 'Title 1',
+        price: 10
+      })
 
+    await request(app)
+      .put(`/api/tickets/${response.body.id}`)
+      .set('Cookie', cookie)
+      .send({
+        title: '',
+        price: 20
+      })
+      .expect(400)
+
+    await request(app)
+      .put(`/api/tickets/${response.body.id}`)
+      .set('Cookie', cookie)
+      .send({
+        title: 'Title',
+        price: -10
+      })
+      .expect(400)
 })
 it('it updates the provided valid inputs', async () => {
 
