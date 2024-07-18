25678c2c24ec94b9acf242c100234f07f8de7390
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-17 21:25:07 -0400

    Remove code used in abstract class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -9,36 +9,13 @@ const client = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 
 client.on('connect', () => {
     console.log('Listener connected to nats')
-    const options = client
-      .subscriptionOptions()
-    // option to manually acknowledge a successful message delivery
-      .setManualAckMode(true)
-      .setDeliverAllAvailable()
-      // create a durable name for listener
-      .setDurableName('order-service')
-    const subscription = client.subscribe(
-        'ticket:created',
-        // create a queue group
-        'orders-service-queue-group',
-        options
-    )
 
-    subscription.on('message', (msg: Message) => {
-       const data = msg.getData()
-
-       // listen for a close event and exit the process
-       client.on('close', () => {
-            console.log('NATS connection closed')
-            process.exit()
-       })
-
-       if(typeof data === 'string') {
-        console.log(`Received event #${msg.getSequence()} with data: ${data}`)
-       }
-       // acknowledgement of message...
-       // if not acknowledged, will continue to send message for processing.
-       msg.ack()
+    // listen for a close event and exit the process
+    client.on('close', () => {
+        console.log('NATS connection closed')
+        process.exit()
     })
+
 })
 
 // listen for SIGINT or SIGTERM and close the client
