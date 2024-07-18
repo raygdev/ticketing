95241ce39457c5a81b26527f2a9e5f01786b489e
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-17 22:33:00 -0400

    Provide better typing around subject and data for given event

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,10 +1,12 @@
 import { Message } from "node-nats-streaming";
 import { Listener } from "./base-listener";
+import { TicketCreatedEvent } from "./ticket-created-event";
+import { Subjects } from "./subjects";
 
-export class TicketCreatedListener extends Listener {
-    subject = 'ticket:created'
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+    readonly subject = Subjects.TicketCreated
     queueGroupName = 'orders-service-queue-group'
-    onMessage(data: any, msg: Message): void {
+    onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
         console.log('Event data!', data)
        // acknowledgement of message...
        // if not acknowledged, will continue to send message for processing.
