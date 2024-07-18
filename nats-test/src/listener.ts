ddec4b563f9b4e66d9f5f21cc7a1809de6f6e0bc
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-17 21:23:52 -0400

    Create concrete class and extend abstract

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -91,3 +91,14 @@ abstract class Listener {
           : JSON.parse(data.toString('utf-8'))
     }
 }
+
+class TicketCreatedListener extends Listener {
+    subject = 'ticket:created'
+    queueGroupName = 'orders-service-queue-group'
+    onMessage(data: any, msg: Message): void {
+        console.log('Event data!', data)
+       // acknowledgement of message...
+       // if not acknowledged, will continue to send message for processing.
+        msg.ack()
+    }
+}
