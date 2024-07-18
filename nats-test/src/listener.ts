1e134f1c837d82e758a461beeb6b66648555922f
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-17 22:14:26 -0400

    Import Listener class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,5 +1,7 @@
-import nats, { Message, Stan } from 'node-nats-streaming'
+import nats from 'node-nats-streaming'
 import { randomBytes } from 'crypto'
+
+import { TicketCreatedListener } from './events/ticket-created-listener'
 
 console.clear()
 
@@ -22,64 +24,3 @@ client.on('connect', () => {
 process.on('SIGINT', () => client.close())
 process.on('SIGTERM', () => client.close())
 
-abstract class Listener {
-    abstract subject: string;
-    abstract queueGroupName: string;
-    abstract onMessage(data: any, msg: Message): void
-    private client: Stan;
-    protected ackWait = 5 * 1000
-
-    constructor(cleint: Stan) {
-        this.client = client
-    }
-
-    subscriptionOptions() {
-        return this.client
-          .subscriptionOptions()
-          .setDeliverAllAvailable()
-          // option to manually acknowledge a successful message delivery
-          .setManualAckMode(true)
-          // set default ack wait time
-          .setAckWait(this.ackWait)
-          // create a durable name for listener
-          .setDurableName(this.queueGroupName)
-
-    }
-
-    listen() {
-        const subscription = this.client.subscribe(
-            this.subject,
-            // create a queue group name
-            this.queueGroupName,
-            this.subscriptionOptions()
-        )
-
-        subscription.on('message', (msg: Message) => {
-            console.log(
-                `Message Received: ${this.subject} / ${this.queueGroupName}`
-            )
-
-            const parsedData = this.parseMessage(msg)
-            this.onMessage(parsedData, msg)
-        })
-    }
-
-    parseMessage(msg: Message) {
-        const data = msg.getData()
-
-        return typeof data === 'string'
-          ? JSON.parse(data)
-          : JSON.parse(data.toString('utf-8'))
-    }
-}
-
-class TicketCreatedListener extends Listener {
-    subject = 'ticket:created'
-    queueGroupName = 'orders-service-queue-group'
-    onMessage(data: any, msg: Message): void {
-        console.log('Event data!', data)
-       // acknowledgement of message...
-       // if not acknowledged, will continue to send message for processing.
-        msg.ack()
-    }
-}
