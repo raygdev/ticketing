c38742a64081685a6e9b9edebb93bcaec32d6a85
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-18 20:58:36 -0400

    Reformat file

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,22 +1,22 @@
-import nats from 'node-nats-streaming'
-import { TicketCreatedPublisher } from './events/ticket-created-publisher'
+import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
-console.clear()
+console.clear();
 
-const client = nats.connect('ticketing', 'abc', {
-    url: 'http://localhost:4222'
-})
+const client = nats.connect("ticketing", "abc", {
+  url: "http://localhost:4222",
+});
 
-client.on('connect', async () => {
-    console.log('Publisher connected to nats')
-    const publisher = new TicketCreatedPublisher(client)
-    try{
-        await publisher.publish({
-            id: '123',
-            title: 'Concert',
-            price: 20
-        })
-    } catch(e) {
-        console.log(e)
-    }
-})
+client.on("connect", async () => {
+  console.log("Publisher connected to nats");
+  const publisher = new TicketCreatedPublisher(client);
+  try {
+    await publisher.publish({
+      id: "123",
+      title: "Concert",
+      price: 20,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+});
