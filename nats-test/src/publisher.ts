b562d31d18d87ba764be6d56a5af3615ed0022cb
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-18 20:51:15 -0400

    Use ticket publisher

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming'
+import { TicketCreatedPublisher } from './events/ticket-created-publisher'
 
 console.clear()
 
@@ -9,13 +10,19 @@ const client = nats.connect('ticketing', 'abc', {
 client.on('connect', () => {
     console.log('Publisher connected to nats')
 
-    const data = JSON.stringify({
-        title: 'Concert',
-        price: 20,
-        id: '123'
-    })
+    // const data = JSON.stringify({
+    //     title: 'Concert',
+    //     price: 20,
+    //     id: '123'
+    // })
 
-    client.publish('ticket:created', data, () => {
-        console.log('Event published')
+    // client.publish('ticket:created', data, () => {
+    //     console.log('Event published')
+    // })
+    const publisher = new TicketCreatedPublisher(client)
+    publisher.publish({
+        id: '123',
+        title: 'Concert',
+        price: 20
     })
 })
