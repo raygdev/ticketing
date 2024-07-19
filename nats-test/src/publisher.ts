4e816c7fbb37cecdc41d9d40fd20717a44963322
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-18 20:58:18 -0400

    Await the published data

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -7,12 +7,16 @@ const client = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222'
 })
 
-client.on('connect', () => {
+client.on('connect', async () => {
     console.log('Publisher connected to nats')
     const publisher = new TicketCreatedPublisher(client)
-    publisher.publish({
-        id: '123',
-        title: 'Concert',
-        price: 20
-    })
+    try{
+        await publisher.publish({
+            id: '123',
+            title: 'Concert',
+            price: 20
+        })
+    } catch(e) {
+        console.log(e)
+    }
 })
