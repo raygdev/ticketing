8f57dde73e3ecf3ee007cf171f786cb7074d9b73
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-21 17:22:09 -0400

    Require authorization and validate body

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,8 +1,22 @@
+import mongoose from 'mongoose'
 import express, { Request, Response } from 'express'
+import { requireAuth, validateRequest } from '@raygdevtickets/common'
+import { body } from 'express-validator'
 
 const router = express.Router()
 
-router.post('/api/orders', async (req: Request, res: Response) => {
+router.post('/api/orders', requireAuth, [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      // not really a good option because of coupling a specific db
+      // implementation. This would fail if using a different db or
+      // switching dbs.
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('Ticket id must be provided')
+
+],
+  async (req: Request, res: Response) => {
     res.send({})
 })
 
