5215a32d0fdd526a178930eac4aa1b0c3173eee9
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-21 17:29:11 -0400

    Add validate middleware and reformat file

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,23 +1,27 @@
-import mongoose from 'mongoose'
-import express, { Request, Response } from 'express'
-import { requireAuth, validateRequest } from '@raygdevtickets/common'
-import { body } from 'express-validator'
+import mongoose from "mongoose";
+import express, { Request, Response } from "express";
+import { requireAuth, validateRequest } from "@raygdevtickets/common";
+import { body } from "express-validator";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/api/orders', requireAuth, [
-    body('ticketId')
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
       .not()
       .isEmpty()
       // not really a good option because of coupling a specific db
       // implementation. This would fail if using a different db or
       // switching dbs.
       .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
-      .withMessage('Ticket id must be provided')
-
-],
+      .withMessage("Ticket id must be provided"),
+  ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    res.send({})
-})
+    res.send({});
+  }
+);
 
-export { router as createOrderRouter}
+export { router as createOrderRouter };
