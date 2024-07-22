f1de22f782c60f8c414c06d1218921c187762a4b
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-21 20:30:40 -0400

    See if an existing order has been created for the ticket

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import express, { Request, Response } from "express";
-import { NotFoundError, requireAuth, validateRequest } from "@raygdevtickets/common";
+import { BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest } from "@raygdevtickets/common";
 import { body } from "express-validator";
 import { Ticket } from "../models/ticket";
 import { Order } from "../models/order";
@@ -22,13 +22,31 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    // Find the ticket the user is trying to order in the database
     const { ticketId } = req.body
 
-    // Make sure the ticket is not already reserved
+    // Find the ticket the user is trying to order in the database
     const ticket = await Ticket.findById(ticketId)
     if(!ticket) {
       throw new NotFoundError()
+    }
+
+    // Make sure the ticket is not already reserved
+    // Run query to look at all orders. Find an order where the ticket
+    // is the ticket found *and* the order status is *not* canceled.
+    // If we find an order from that means the ticket *is* reserved
+    const existingOrder = await Order.findOne({
+      ticket: ticket,
+      status: {
+        $in: [
+          OrderStatus.Created,
+          OrderStatus.AwaitingPayment,
+          OrderStatus.Complete
+        ]
+      }
+    })
+
+    if(existingOrder) {
+      throw new BadRequestError("Ticket is already reserved.")
     }
 
     // Caculate expiration date for this order
