9affc8fe35c9cd659c5aca500ce9d778cdab9316
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-22 10:59:01 -0400

    Create ticket updated listener

---
+++ orders/src/events/listeners/ticket-updated-listener.ts
@@ -0,0 +1,20 @@
+import { TicketUpdatedEvent, Listener, Subjects } from "@raygdevtickets/common";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
+import { queueGroupName } from "./queue-group-name";
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+    readonly subject = Subjects.TicketUpdated;
+    queueGroupName = queueGroupName
+    async onMessage(data: TicketUpdatedEvent['data'], msg: Message): Promise<void> {
+        const { title, price } = data
+        const ticket = await Ticket.findById(data.id)
+
+        if(!ticket) {
+          throw new Error('Ticket not found')
+        }
+
+        ticket.set({ title, price })
+        await ticket.save()
+    }
+}
