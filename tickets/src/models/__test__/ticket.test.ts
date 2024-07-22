e55465d04a9e01ed9ff8636e5d98faed0425cca2
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-22 13:41:10 -0400

    Assert that second instance fails on save

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -1,5 +1,28 @@
 import { Ticket } from "../ticket";
 
 it('implements optimistic concurrency control', async () => {
-    
+  // create an instance of a ticket
+  const ticket = Ticket.build({
+    title: "Concert",
+    price: 20,
+    userId: '123'
+  })
+
+  // save the ticket to the db
+  await ticket.save()
+
+  // fetch the ticket twice
+  const firstInstance = await Ticket.findById(ticket.id)
+  const secondInstance = await Ticket.findById(ticket.id)
+
+  //make two separate changes to the tickets we fetch
+  firstInstance!.set({ price: 10 })
+  secondInstance!.set({ price: 10 })
+
+  // save the first fetched ticket and expect success
+  await expect(firstInstance!.save()).rejects.not.toThrow()
+
+  // save the second fetched ticket and expect an error
+  await expect(secondInstance!.save()).rejects.toThrow()
+  
 })
