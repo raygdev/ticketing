4180ff4870a500f59d956742b28f128f5ef55819
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-21 21:21:42 -0400

    Add more tests for order

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,6 +1,8 @@
 import request from "supertest"
 import { app } from "../../app"
 import mongoose from "mongoose"
+import { Order, OrderStatus } from "../../models/order"
+import { Ticket } from "../../models/ticket"
 
 it('returns an error if ticket does not exist', async () => {
   const ticketId = new mongoose.Types.ObjectId()
@@ -12,8 +14,40 @@ it('returns an error if ticket does not exist', async () => {
     .expect(404)
 })
 it('returns an error if ticket is already reserved', async () => {
+  const ticket = Ticket.build({
+    title: 'Concert',
+    price: 20
+  })
 
+  await ticket.save()
+  const order = Order.build({
+    ticket,
+    userId: '12t24',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  })
+
+  await order.save()
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', signin())
+    .send({
+        ticketId: ticket.id
+    })
+    .expect(400)
 })
 it('reserves a ticket', async () => {
+  const ticket = Ticket.build({
+    title: 'Concert',
+    price: 20
+  })
 
+  await ticket.save()
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', signin())
+    .send({ ticketId: ticket.id })
+    .expect(201)
 })
