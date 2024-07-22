fc2d2666ea214215d7c2f38dbf5ef59b7aea777e
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-22 10:51:39 -0400

    Use id property for building ticket

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -7,9 +7,9 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
     readonly subject = Subjects.TicketCreated;
     queueGroupName = queueGroupName
     async onMessage(data: TicketCreatedEvent['data'], msg: Message): Promise<void> {
-      const { title, price } =  data;
+      const { id, title, price } =  data;
       const ticket = Ticket.build({
-        title, price
+        id, title, price
       })
       await ticket.save()
 
