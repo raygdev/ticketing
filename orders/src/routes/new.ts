4e95951ee7f1f9c5bf8d95ed800a37745c4f3927
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-21 20:52:14 -0400

    Create expiry, build, and save order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -7,6 +7,8 @@ import { Order } from "../models/order";
 
 const router = express.Router();
 
+const EXPIRATION_WINDOW_SECONDS = 15 * 60
+
 router.post(
   "/api/orders",
   requireAuth,
@@ -38,11 +40,22 @@ router.post(
     }
 
     // Caculate expiration date for this order
+    const expiration = new Date()
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS)
 
     // Build the order and save it to the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket: ticket
+    })
+
+    await order.save()
 
     // Publish an event saying an order was created
-    res.send({});
+
+    res.status(201).send(order);
   }
 );
 
