0aed45b61da4c07b78860992b21a6a743f9173df
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-22 21:27:53 -0400

    Update ticket model to user order id

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -11,6 +11,7 @@ interface TicketDoc extends mongoose.Document {
     price: number
     userId: string
     version: number
+    orderId?: string
 }
 interface TicketModel extends mongoose.Model<TicketDoc>{
     build(attrs: TicketAttrs): TicketDoc
@@ -28,6 +29,9 @@ const ticketSchema = new mongoose.Schema({
     userId: {
         type: String,
         required: true
+    },
+    orderId: {
+        type: String
     }
 }, {
     toJSON: {
