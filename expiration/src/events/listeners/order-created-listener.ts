5b972a624717c1b13b3070e783b7592240706c09
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-26 22:09:57 -0400

    Setup initial listener for queue

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,11 +1,13 @@
 import { Listener, OrderCreatedEvent, OrderStatus, Subjects } from "@raygdevtickets/common";
 import { Message } from "node-nats-streaming";
 import { queueGroupName } from "./queue-group-name";
+import { expirationQueue } from "../../queues/expiration-queue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName
   async onMessage(data: OrderCreatedEvent['data'], msg: Message): Promise<void> {
-    
+    await expirationQueue.add({ orderId: data.id })
+    msg.ack()
   }
 }
