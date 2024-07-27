6c4d2b5dfb570d282f53adefd827a0e92901a61a
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-26 20:58:48 -0400

    Setup boiler plate for expiration

---
+++ expiration/src/index.ts
@@ -0,0 +1,33 @@
+import { natsWrapper } from "./nats-wrapper"
+
+const start = async () => {
+    if(!process.env.NATS_URL) {
+        throw new Error('No NATS url defined')
+    }
+
+    if(!process.env.NATS_CLUSTER_ID) {
+        throw new Error('No NATS cluster id defined')
+    }
+
+    if(!process.env.NATS_CLIENT_ID) {
+        throw new Error('No NATS client id defined')
+    }
+    console.log(process.env.NATS_URL)
+
+    try {
+        await natsWrapper.connect(
+            process.env.NATS_CLUSTER_ID,
+            process.env.NATS_CLIENT_ID,
+            process.env.NATS_URL
+        )
+        natsWrapper.client.on('close', () => {
+            console.log('NATS connection closed!')
+            process.exit()
+        })
+        process.on('SIGINT', () => natsWrapper.client.close())
+        process.on('SIGTERM', () => natsWrapper.client.close())
+    } catch(e) {
+        console.error(e)
+    }
+}
+start()
