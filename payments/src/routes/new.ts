d479c8a2ae2c42b93144a7ea4f8094e303e0ce0f
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-28 19:58:25 -0400

    Add checks for payment

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -5,6 +5,8 @@ import {
   validateRequest,
   BadRequestError,
   NotFoundError,
+  NotAuthorizedError,
+  OrderStatus
 } from "@raygdevtickets/common";
 import { Order } from "../models/order";
 
@@ -25,7 +27,23 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    res.send({ success: true });
+    const { token, orderId } = req.body
+
+    const order = await Order.findById(orderId)
+
+    if(!order) {
+        throw new NotFoundError()
+    }
+
+    if(order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError()
+    }
+
+    if(order.status === OrderStatus.Canceled) {
+      throw new BadRequestError('Cannot pay for a canceled order')
+    }
+
+    res.send({ succes: true })
   }
 );
 
