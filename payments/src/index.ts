89b0dc7ab6551f04d2cf429fbfcbf4762bf18fd0
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-28 18:55:34 -0400

    Listen for order events

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
 import { app } from './app'
 import { natsWrapper } from './nats-wrapper'
+import { OrderCanceledListener } from './events/listeners/order-canceled-listener'
+import { OrderCreatedListener } from './events/listeners/order-created-listener'
 
 const start = async () => {
     if(!process.env.JWT_KEY) {
@@ -35,6 +37,9 @@ const start = async () => {
         })
         process.on('SIGINT', () => natsWrapper.client.close())
         process.on('SIGTERM', () => natsWrapper.client.close())
+
+        new OrderCreatedListener(natsWrapper.client).listen()
+        new OrderCanceledListener(natsWrapper.client).listen()
         
         await mongoose.connect(process.env.MONGO_URI)
         console.log("Connected to mongodb")
