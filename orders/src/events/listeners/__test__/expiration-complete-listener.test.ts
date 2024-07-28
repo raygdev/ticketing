f4b50aafef65539e612964f246ec68b9fa084cf7
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-28 14:26:26 -0400

    Assert listener

--- orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
+++ orders/src/events/listeners/__test__/expiration-complete-listener.test.ts
@@ -34,4 +34,36 @@ const setup = async () => {
     ack: jest.fn()
   }
 
+  return { ticket, order, data, msg, listener }
+
 }
+
+it('updates the order status to canceled', async () => {
+  const { listener, order, ticket, data, msg } = await setup()
+
+  await listener.onMessage(data,msg)
+
+  const updatedOrder = await Order.findById(order.id)
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.Canceled)
+})
+it('emit an OrderCanceled event', async () => {
+  const { listener, order, data, msg } = await setup()
+
+  await listener.onMessage(data,msg)
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled()
+
+  const eventData = JSON.parse(
+    (natsWrapper.client.publish as jest.Mock).mock.calls[0][1]
+  )
+
+  expect(eventData.id).toEqual(order.id)
+})
+it('acks the message', async () => {
+  const { listener, data, msg } = await setup()
+
+  await listener.onMessage(data,msg)
+
+  expect(msg.ack).toHaveBeenCalled()
+})
