c588ae098b80f29d0c7db931e89873687917d5f6
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-28 16:15:26 -0400

    Create orders model for payments

---
+++ payments/src/models/order.ts
@@ -0,0 +1,56 @@
+import mongoose, { version } from "mongoose";
+import { OrderStatus } from "@raygdevtickets/common";
+
+interface OrderAttrs {
+  id: string
+  status: OrderStatus
+  price: number
+  version: number
+  userId: string
+}
+
+interface OrderDoc extends mongoose.Document {
+  version: number
+  userId: string
+  price: number
+  status: OrderStatus
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc
+}
+
+const orderSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: Object.values(OrderStatus)
+    }
+},{
+    toJSON: {
+        transform(doc, ret) {
+            ret.id = ret._id
+            delete ret._id
+        }
+    }
+})
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+    return new Order({
+        _id: attrs.id,
+        version: attrs.version,
+        userId: attrs.userId,
+        status: attrs.status,
+        price: attrs.price
+    })
+}
+
+const Order = mongoose.model<OrderDoc, OrderModel>('orders', orderSchema)
