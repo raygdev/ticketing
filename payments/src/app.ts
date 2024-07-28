0c6294446cf0e2462d0e5f4a38a1fb35821ff1b8
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-28 19:47:01 -0400

    Import new router and use currentUser

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import 'express-async-errors'
 import cookieSession from 'cookie-session'
 import { errorHandler, NotFoundError, currentUser } from '@raygdevtickets/common'
+import { createChargeRouter } from './routes/new'
 
 
 const app = express()
@@ -11,6 +12,9 @@ app.use(cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== 'test'
 }))
+
+app.use(currentUser)
+app.use(createChargeRouter)
 
 
 app.all("*", async () => {
