76203d7c79322d6b6b4714d7272f178dc9c372fe
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-28 20:23:59 -0400

    Assert 404 when order not found

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -1,12 +1,20 @@
 import request from 'supertest'
 import { app } from '../../app'
+import mongoose from 'mongoose'
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
-
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', signin())
+    .send({
+        token: 'alsdfjal',
+        orderId: new mongoose.Types.ObjectId().toHexString()
+    })
+    .expect(404)
 })
 it('returns a 401 when purchasing an order that does not belong to the user', async () => {
 
 })
 it('returns a 400 when purchasing a canceled order', async () => {
-    
+
 })
