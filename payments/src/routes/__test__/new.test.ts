eec58c41c0a5b1be562377fef0d43d6e5d704cd3
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-28 20:29:03 -0400

    Assert 401 with wrong user

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -1,6 +1,8 @@
 import request from 'supertest'
 import { app } from '../../app'
 import mongoose from 'mongoose'
+import { Order } from '../../models/order'
+import { OrderStatus } from '@raygdevtickets/common'
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
   await request(app)
@@ -13,7 +15,23 @@ it('returns a 404 when purchasing an order that does not exist', async () => {
     .expect(404)
 })
 it('returns a 401 when purchasing an order that does not belong to the user', async () => {
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created
+  })
+  await order.save()
 
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', signin())
+    .send({
+        token: 'asdfasdf',
+        orderId: order.id
+    })
+    .expect(401)
 })
 it('returns a 400 when purchasing a canceled order', async () => {
 
