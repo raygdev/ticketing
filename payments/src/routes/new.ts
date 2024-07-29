ba929480186a9ecf9755dc3c97a6912db2668ec8
raygdev <101662915+raygdev@users.noreply.github.com>
2024-07-28 21:27:27 -0400

    Await charge in new payment

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
   OrderStatus
 } from "@raygdevtickets/common";
 import { Order } from "../models/order";
+import { stripe } from "../stripe";
 
 const router = express.Router();
 
@@ -42,6 +43,12 @@ router.post(
     if(order.status === OrderStatus.Canceled) {
       throw new BadRequestError('Cannot pay for a canceled order')
     }
+
+    await stripe.charges.create({
+        currency: 'usd',
+        amount: (order.price * 100),
+        source: token
+    })
 
     res.send({ succes: true })
   }
