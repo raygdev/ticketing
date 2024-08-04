3318df483a1dff53fa85025a76621cc75554fcc8
raygdev <101662915+raygdev@users.noreply.github.com>
2024-08-04 18:08:59 -0400

    Create state for price and title

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,15 +1,20 @@
+import { useState } from "react"
+
 const NewTicket = () => {
+  const [title, setTitle] = useState('')
+  const [price, setPrice] = useState('')
+
   return (
     <div>
         <h1>Create a Ticket</h1>
         <form className="d-flex flex-column gap-3">
             <div className="form-group">
                 <label>Title</label>
-                <input className="form-control" />
+                <input value={title} onChange={(e) => setTitle(e.target.value)} className="form-control" />
             </div>
             <div className="form-group">
                 <label>Price</label>
-                <input className="form-control" />
+                <input value={price} onChange={(e) => setPrice(e.target.value)} className="form-control" />
             </div>
             <button className="btn btn-primary align-self-start">Submit</button>
         </form>
