36f906b88cdb2a78a15396db96685218dccdd165
raygdev <101662915+raygdev@users.noreply.github.com>
2024-08-04 18:09:23 -0400

    Reformat file

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,25 +1,33 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const NewTicket = () => {
-  const [title, setTitle] = useState('')
-  const [price, setPrice] = useState('')
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
 
   return (
     <div>
-        <h1>Create a Ticket</h1>
-        <form className="d-flex flex-column gap-3">
-            <div className="form-group">
-                <label>Title</label>
-                <input value={title} onChange={(e) => setTitle(e.target.value)} className="form-control" />
-            </div>
-            <div className="form-group">
-                <label>Price</label>
-                <input value={price} onChange={(e) => setPrice(e.target.value)} className="form-control" />
-            </div>
-            <button className="btn btn-primary align-self-start">Submit</button>
-        </form>
+      <h1>Create a Ticket</h1>
+      <form className="d-flex flex-column gap-3">
+        <div className="form-group">
+          <label>Title</label>
+          <input
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label>Price</label>
+          <input
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary align-self-start">Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default NewTicket
+export default NewTicket;
