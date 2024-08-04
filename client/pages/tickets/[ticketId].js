df360cd76bd921c3731d791361ac42f081a45b06
raygdev <101662915+raygdev@users.noreply.github.com>
2024-08-04 19:53:24 -0400

    Route to order on successful creation

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,11 +1,12 @@
 import useRequest from "../../hooks/useRequest"
+import Router from "next/router"
 
 const TicketShow = ({ ticket }) => {
     const { doRequest, errors } = useRequest({
         url: '/api/orders',
         method: 'post',
         body: { ticketId: ticket.id },
-        onSuccess: (data) => console.log(data)
+        onSuccess: (order) => Router.push(`/orders/[orderId]`, `/orders/${order.id}`)
     })
 
     return (
