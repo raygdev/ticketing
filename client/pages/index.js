ceab1fde723154b641a6e69a0cdee0c8823e7b9f
raygdev <101662915+raygdev@users.noreply.github.com>
2024-08-03 20:46:04 -0400

    Receive client and currentUser as arguments

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,11 @@
-import buildClient from "../api/build-client"
-
 const Landing = ({ currentUser }) => {
     return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>
 }
 
-Landing.getInitialProps = async (context) => {
-    const client = buildClient(context)
-    const { data } = await client.get('/api/users/currentuser')
-    return data
+Landing.getInitialProps = async (context, client, currentUser) => {
+    // const client = buildClient(context)
+    // const { data } = await client.get('/api/users/currentuser')
+    return {}
 }
 
 export default Landing
