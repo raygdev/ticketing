2a55c9b91cf9675eb998e6df4828814b410655ae
raygdev <101662915+raygdev@users.noreply.github.com>
2024-08-04 19:52:44 -0400

    Create order component for payments

---
+++ client/pages/orders/[orderId].js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+
+const OrderShow = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState(0)
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date()
+      setTimeLeft(Math.round(msLeft / 1000))
+    }
+    findTimeLeft()
+    const timerId = setInterval(findTimeLeft, 1000)
+
+    return () => clearInterval(timerId)
+  },[])
+
+  if(timeLeft < 0) {
+    return <div>Order Expired</div>
+  }
+
+
+  return (
+    <div>
+        <div>Time left to pay: {timeLeft} seconds</div>
+        <h1>{order.ticket.title}</h1>
+        <h4>{order.status}</h4>
+    </div>
+  )
+}
+
+OrderShow.getInitialProps = async (context, client) => {
+    const { orderId } = context.query
+    const { data } = await client.get(`/api/orders/${orderId}`)
+
+    return { order: data }
+}
+
+export default OrderShow
